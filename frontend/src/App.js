f821e4d15eb1760a727d3f13a7bcd97f13f6316e
Marty Sloan <marty@karmatek.io>
2022-05-24 16:53:17 -0400

    add table to ucm list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import Container from 'react-bootstrap/Container';
+import {Col, Container, Row, Table} from "react-bootstrap";
 import TopNav from "./Layout/TopNav";
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
+import {LinkContainer} from "react-router-bootstrap";
 
 function App() {
     const [ucms, setUcms] = useState([]);
@@ -14,9 +15,32 @@ function App() {
   return (
       <Container fluid>
           <TopNav />
-          {ucms.map(ucm => {
-              return <p>{ucm.name}</p>
-          })}
+          <Row className="justify-content-md-center mt-5">
+              <Col xs={12} md={10} lg={8}>
+                  <Table responsive bordered hover>
+                      <thead>
+                      <tr>
+                          <th>Name</th>
+                          <th>IP Address</th>
+                          <th>Username</th>
+                          <th>Version</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      {ucms.map((ucm, index) => {
+                          return (
+                              <tr key={index}>
+                                  <td><a href="#">{ucm.name}</a></td>
+                                  <td>{ucm.ipAddress}</td>
+                                  <td>{ucm.username}</td>
+                                  <td>{ucm.version}</td>
+                              </tr>
+                          )
+                      })}
+                      </tbody>
+                  </Table>
+              </Col>
+          </Row>
       </Container>
   );
 }
---
+++ frontend/src/Pages/Ucm/EditUcm.js
@@ -0,0 +1,7 @@
+const EditUcm = () => {
+    return (
+        <h1>Foo</h1>
+    )
+}
+
+export default EditUcm
