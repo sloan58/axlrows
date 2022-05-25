99fd621284277945cb096aa607222a524630fa84
Marty Sloan <marty@karmatek.io>
2022-05-25 13:35:30 -0400

    add loading indicator to ucms table

--- frontend/src/Pages/Ucm/Ucm.js
+++ frontend/src/Pages/Ucm/Ucm.js
@@ -1,20 +1,93 @@
 import { useParams } from "react-router-dom";
 import {useEffect, useState} from "react";
 import request from '../../util/request'
+import {Button, Col, Form, Row} from "react-bootstrap";
 
 const Ucm = () => {
     let params = useParams();
-    const [ucm, setUcm] = useState([]);
+    const [input, setInput] = useState({});
+
     useEffect(() => {
-        console.log(request.get(`/ucm/${params.ucmId}`)
-            .then(data => data.json())
-            .then(ucm => {console.log('here i am'); setUcm(ucm)})
+        request(`/ucm/${params.ucmId}`)
+            .then(ucm => setInput(ucm))
             .catch(err => console.log(err))
-        )
     }, [])
 
+    const handleSubmit = e => {
+        e.preventDefault()
+        // request(`/ucm/${params.ucmId}`)
+        //     .then(ucm => setInput(ucm))
+        //     .catch(err => console.log(err))
+        // history.push('/admin/ucms')
+    }
+
     return (
-        <h1>{ucm.name}</h1>
+        <Row className="justify-content-md-center mt-5">
+            <Col xs={12} md={4}>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text"
+                                      id="name"
+                                      placeholder=""
+                                      value={input.name}
+                                      onChange={e =>
+                                          setInput({ ...input, [e.target.id]: e.target.value })
+                                      }
+                                      required
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>IP Address</Form.Label>
+                        <Form.Control type="text"
+                                      id="ipAddress"
+                                      placeholder=""
+                                      value={input.ipAddress}
+                                      onChange={e =>
+                                          setInput({ ...input, [e.target.id]: e.target.value })
+                                      }
+                                      required
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text"
+                                      id="username"
+                                      placeholder=""
+                                      value={input.username}
+                                      onChange={e =>
+                                          setInput({ ...input, [e.target.id]: e.target.value })
+                                      }
+                                      required
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password"
+                                      id="password"
+                                      placeholder=""
+                                      value={input.password}
+                                      onChange={e =>
+                                          setInput({ ...input, [e.target.id]: e.target.value })
+                                      }
+                                      required
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Version</Form.Label>
+                        <Form.Select aria-label="Default select example">
+                            <option>Select</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Col>
+        </Row>
     )
 }
 
--- frontend/src/Pages/Ucm/Ucms.js
+++ frontend/src/Pages/Ucm/Ucms.js
@@ -1,4 +1,4 @@
-import {Col, Row, Table} from "react-bootstrap";
+import {Button, Col, Row, Spinner, Table} from "react-bootstrap";
 import {useEffect, useState} from "react";
 import {LinkContainer} from "react-router-bootstrap";
 import request from '../../util/request'
@@ -6,40 +6,57 @@ import request from '../../util/request'
 const Ucms = () => {
 
     const [ucms, setUcms] = useState([]);
+    const [isLoading, setIsLoading] = useState(true)
+
     useEffect(() => {
-        console.log(request('/ucm')
-            .then(ucms => setUcms(ucms))
-            .catch(err => console.log(err))
-        )
+        request('/ucm')
+            .then(ucms => {
+                setUcms(ucms)
+                setIsLoading(false)
+            }).catch(err => console.log(err))
     }, [])
 
     return (
-        <Row className="justify-content-md-center mt-5">
+        <Row className="justify-content-md-center mt-5 text-center">
             <Col xs={12} md={10} lg={8}>
-                <Table responsive bordered hover>
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>IP Address</th>
-                        <th>Username</th>
-                        <th>Version</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {ucms.map((ucm, index) => {
-                        return (
-                            <tr key={index}>
-                                <LinkContainer to={`/ucm/${ucm.id}`} style={{ cursor: "pointer"}}>
-                                    <td className="text-primary">{ucm.name}</td>
-                                </LinkContainer>
-                                <td>{ucm.ipAddress}</td>
-                                <td>{ucm.username}</td>
-                                <td>{ucm.version}</td>
-                            </tr>
-                        )
-                    })}
-                    </tbody>
-                </Table>
+                {isLoading ? (
+                    <Button variant="primary" disabled>
+                        <Spinner
+                            as="span"
+                            animation="grow"
+                            size="sm"
+                            role="status"
+                            aria-hidden="true"
+                            className="me-2"
+                        />
+                        Loading...
+                    </Button>
+                ) : (
+                    <Table responsive bordered hover>
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>IP Address</th>
+                            <th>Username</th>
+                            <th>Version</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {ucms.map((ucm, index) => {
+                            return (
+                                <tr key={index}>
+                                    <LinkContainer to={`/ucm/${ucm.id}`} style={{ cursor: "pointer"}}>
+                                        <td className="text-primary">{ucm.name}</td>
+                                    </LinkContainer>
+                                    <td>{ucm.ipAddress}</td>
+                                    <td>{ucm.username}</td>
+                                    <td>{ucm.version}</td>
+                                </tr>
+                            )
+                        })}
+                        </tbody>
+                    </Table>
+                )}
             </Col>
         </Row>
     )
