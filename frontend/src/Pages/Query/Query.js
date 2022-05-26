82246feb6ef02a0b4fb1838e11154b79ac1cf4d5
Marty Sloan <marty@karmatek.io>
2022-05-26 15:09:15 -0400

    add Query.js

---
+++ frontend/src/Pages/Query/Query.js
@@ -0,0 +1,13 @@
+import {Col, Row} from "react-bootstrap";
+
+const Query = () => {
+    return (
+        <Row className="justify-content-md-center mt-5">
+            <Col xs={12} md={4}>
+
+            </Col>
+        </Row>
+    )
+}
+
+export default Query
