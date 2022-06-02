3cf251cbffb5a98ca3db9c8acf2c5b7eaee15305
Marty Sloan <marty@karmatek.io>
2022-06-02 07:15:25 -0400

    add download button

--- frontend/src/Components/QueryResults.js
+++ frontend/src/Components/QueryResults.js
@@ -64,17 +64,28 @@ const QueryResults = () => {
             {state.query_results.length !== 0 && (
                 <>
                     <Row className="justify-content-md-center mt-5">
-                        <Col xs={5} className="text-start">
-                            <Col xs={7} className="text-start">
-                                <Form.Control onChange={updateSearch} type="email" placeholder={state.results_search === '' ? 'Search' : state.results_search} />
-                            </Col>
-                        </Col>
-                        <Col xs={5} className="text-end">
-                            <DropdownButton title={state.pagination_length} id="bg-vertical-dropdown-1">
-                                <Dropdown.Item onClick={() => onSelect(10)}>10</Dropdown.Item>
-                                <Dropdown.Item onClick={() => onSelect(25)}>25</Dropdown.Item>
-                                <Dropdown.Item onClick={() => onSelect(50)}>50</Dropdown.Item>
-                            </DropdownButton>
+                        <Col xs={10}>
+                            <Row className="justify-content-md-center">
+                                <Col className="text-start">
+                                    <Form.Control onChange={updateSearch} type="email" placeholder={state.results_search === '' ? 'Search' : state.results_search} />
+                                </Col>
+                                <Col className="d-inline-flex justify-content-end">
+                                    <div className="mt-2 text-success" style={{cursor: 'pointer'}}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                             className="bi bi-download" viewBox="0 0 16 16">
+                                            <path
+                                                d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                                            <path
+                                                d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                                        </svg>
+                                    </div>
+                                    <DropdownButton title={state.pagination_length} id="bg-vertical-dropdown-1" className="ms-3">
+                                        <Dropdown.Item onClick={() => onSelect(10)}>10</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => onSelect(25)}>25</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => onSelect(50)}>50</Dropdown.Item>
+                                    </DropdownButton>
+                                </Col>
+                            </Row>
                         </Col>
                     </Row>
                     <Row className="justify-content-md-center mt-3">
