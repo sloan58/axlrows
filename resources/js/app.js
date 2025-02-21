9bdb2479fd62da3aa7591a2d4816f5296737cfda
Marty Sloan <marty@karmatek.io>
2025-02-20 19:14:25 -0500

    Add Luvi UI

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+
+import collapse from "@alpinejs/collapse";
+import anchor from "@alpinejs/anchor";
+
+document.addEventListener(
+    "alpine:init",
+    () => {
+        const modules = import.meta.glob("./plugins/**/*.js", { eager: true });
+
+        for (const path in modules) {
+            window.Alpine.plugin(modules[path].default);
+        }
+        window.Alpine.plugin(collapse);
+        window.Alpine.plugin(anchor);
+    },
+    { once: true },
+);
