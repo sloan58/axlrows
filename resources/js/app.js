481489396d0ef35ace945676beb7db9d4a24ff22
Marty Sloan <marty@karmatek.io>
2025-02-23 19:30:18 -0500

    Add vite-livewire-plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
 import './bootstrap';
 import { basicEditor } from "prism-code-editor/setups"
 import "prism-code-editor/prism/languages/sql"
+import { livewire_hot_reload } from 'virtual:livewire-hot-reload'
 
 import collapse from "@alpinejs/collapse";
 import anchor from "@alpinejs/anchor";
+
+livewire_hot_reload();
 
 document.addEventListener(
     "alpine:init",
